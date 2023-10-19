0d0df199b273a8dc31fb4aa2b6bcd69e41b47b60
MarcinGladkowski <marcingladkowski@gmail.com>
2023-10-19 22:02:29 +0200

    jwt: authenticate by API JWT using Lexik bundle

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/api', 'api_')]
 class AuthController
 {
     #[Route('/check', name: 'check')]
